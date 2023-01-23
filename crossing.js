b314ffe8f99e84cac17b1e5f8ed17f6be70431d4
Sarah-Lilly <117541687+Sarah-Lilly@users.noreply.github.com>
2023-01-23 21:35:27 +0100

    Update crossing.js

--- crossing.js
+++ crossing.js
@@ -3,6 +3,7 @@ $(function() {
 
 
 //das ist die Codierung für den Übergang von der Startseite zu der Frage-Seite mittels eines Klicks auf den Weiter-Button//
+// zusätzlich kann jede Antwort ausgewählt (selektiert) werden, so oft wie es gewollt wird und die angeklickte Antwortmöglichkeit wird dunkelblau gefärbt. // 
 $(".start").click(function() {
 	console.log("Start");
 	$(".quiz_start").fadeOut();
@@ -13,3 +14,46 @@ function startQuiz() {
 	showNextQuestion();
 	$("#question").fadeIn();
 }
+
+
+function selectAnswer(id) {
+	$(id).addClass("btn-primary");
+	$(id).removeClass("btn-default");
+}
+
+function deselectAnswer(id) {
+	$(id).addClass("btn-default");
+	$(id).removeClass("btn-primary");
+}
+
+$("#answer_a_btn").click(function() {
+	selectAnswer("#answer_a_btn");
+	deselectAnswer("#answer_b_btn");
+	deselectAnswer("#answer_c_btn");
+	deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_b_btn").click(function() {
+	deselectAnswer("#answer_a_btn");
+	selectAnswer("#answer_b_btn");
+	deselectAnswer("#answer_c_btn");
+	deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_c_btn").click(function() {
+	deselectAnswer("#answer_a_btn");
+	deselectAnswer("#answer_b_btn");
+	selectAnswer("#answer_c_btn");
+	deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_d_btn").click(function() {
+	deselectAnswer("#answer_a_btn");
+	deselectAnswer("#answer_b_btn");
+	deselectAnswer("#answer_c_btn");
+	selectAnswer("#answer_d_btn");
+});
+
+$("#answer_commit_btn").click(function() {
+	validateAnswer();
+});
