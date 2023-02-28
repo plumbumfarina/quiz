878181ea35060ceb35a225828cf0cfaaea8ee7d9
Sarah-Lilly <117541687+Sarah-Lilly@users.noreply.github.com>
2023-02-28 23:34:00 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -31,22 +31,28 @@ var questions = [
 	}		
 ]; 
 
+
 function showNextQuestion() {
+	if (currentQuestionNo >= questions.length) {
+		showEnd();
+		currentQuestionNo = 0;
+	}
+	
 	$(".answer").removeClass("btn-primary btn-danger btn-success btn-default");
 	$(".answer").addClass("btn-default");
-	
+
 	console.log("Loading Question:" + currentQuestionNo);
-	currentQuestion = questions[currentQuestionNo];
-	
+  currentQuestion = questions[currentQuestionNo];
+
 	$("#qno").text(currentQuestionNo + 1);
-	$("#question_text").text(currentQuestion.question);
-	$("#answer_a").text(currentQuestion.answers.A);
-	$("#answer_b").text(currentQuestion.answers.B);
-	$("#answer_c").text(currentQuestion.answers.C);
-	$("#answer_d").text(currentQuestion.answers.D);
-} 
+  $("#question_text").text(currentQuestion.question);
+  $("#answer_a").text(currentQuestion.answers.A);
+  $("#answer_b").text(currentQuestion.answers.B);
+  $("#answer_c").text(currentQuestion.answers.C);
+  $("#answer_d").text(currentQuestion.answers.D);
+}
 
 function getRightAnswer() {
-	return currentQuestion.right;
+  return currentQuestion.right;
 }
 
