4fe293e7ddd710c50891a9a2b9841450a947fd71
Sarah-Lilly <117541687+Sarah-Lilly@users.noreply.github.com>
2023-03-05 13:43:05 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -33,24 +33,21 @@ var questions = [
 
 
 function showNextQuestion() {
-	if (currentQuestionNo >= questions.length) {
-		showEnd();
-		currentQuestionNo = 0;
-	}
-	
-	$(".answer").removeClass("btn-primary btn-danger btn-success btn-default");
+if (currentQuestionNo >= questions.length) {
+    showEnd();
+    currentQuestionNo = 0;  }  
+	console.log("Loading Question:" + currentQuestionNo);  
+	currentQuestion = questions[currentQuestionNo];  
+	$("#qno").text(currentQuestionNo+1);  
+	$("#question_text").text(currentQuestion.question);  
+	$("#answer_a").text(currentQuestion.answers.A);  
+	$("#answer_b").text(currentQuestion.answers.B);  
+	$("#answer_c").text(currentQuestion.answers.C);  
+	$("#answer_d").text(currentQuestion.answers.D);   
+	$(".answer").removeClass("btn-primary btn-danger btn-success btn-default");  
 	$(".answer").addClass("btn-default");
-
-	console.log("Loading Question:" + currentQuestionNo);
-  currentQuestion = questions[currentQuestionNo];
-
-	$("#qno").text(currentQuestionNo + 1);
-  $("#question_text").text(currentQuestion.question);
-  $("#answer_a").text(currentQuestion.answers.A);
-  $("#answer_b").text(currentQuestion.answers.B);
-  $("#answer_c").text(currentQuestion.answers.C);
-  $("#answer_d").text(currentQuestion.answers.D);
 }
+
 
 function getRightAnswer() {
   return currentQuestion.right;
