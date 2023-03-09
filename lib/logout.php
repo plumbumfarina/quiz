64022fd79c557909b4ad10857816625ae10a7aa7
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-09 17:13:39 +0100

    aktueller Stand Lukas

--- lib/logout.php
+++ lib/logout.php
@@ -9,7 +9,7 @@ $_SESSION = array();
 session_destroy();
 
 // redirect auf die Loginseite nach 10 sec
-header("Refresh: 5; URL=login.php");
+header("Refresh: 5; URL=../login.php");
 echo "Logout erfolgreich. In 5 Sekunden wird die Login-Seite wieder angezeigt.";
 
 ?>
