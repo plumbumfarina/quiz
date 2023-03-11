feffedbd396dc2ea4981eb769e7ddb85c7776cac
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-11 16:22:25 +0100

    Add files via upload

--- decks.php
+++ decks.php
@@ -18,6 +18,8 @@ include('lib/getFragenNumber.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Decks</title>
 </head>
 <body>
@@ -26,34 +28,122 @@ include('lib/getFragenNumber.php');
 ?>
 
 
-<div class=container__login">
-    <h1 class="form__title">Fragendecks</h1>
-    <table>
-        <tr>
-            <th>Deckname</th>
-            <th>Kursname</th>
-            <th>Anzahl Fragen</th>
-        </tr>
-        <tr>
-            <td><?php echo $fragendeck_name; ?></td>
-            <td><?php echo $modulname; ?></td>
-            <td><?php echo $fragenAnzahl; ?></td>
-        </tr>
-        <tr>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-        </tr>
-    </table>
+<div class=container__login>
+    <div class="container mt-3">
+        <h1 class="form__title">Fragendecks</h1>
+            <table class="table table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Deckname</th>
+                        <th>Modulkürzel</th>
+                        <th>Modulname</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "toor";
+                        $dbname = "quiz";
+                        $user_id = $_SESSION['userid'];
 
-<div>
-<p><?php echo $fragendeck_id; ?></p>
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        $sql = "SELECT fragendeck_name, fragendeck.modul_id, modulkuerzel, modulname FROM fragendeck JOIN modul WHERE (fragendeck.modul_id = modul.modul_id) AND (user_id = $user_id)" ;
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr><td>" . $row["fragendeck_name"]. "</td><td>" . $row["modulkuerzel"]. "</td><td>" . $row["modulname"]. "</td></tr>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+
+                        $conn->close();
+                    ?>
+                </tbody>
+            </table>
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="btn btn-outline-success"> Hinzufügen
+                    </button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-warning"> Bearbeiten
+                    </button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-danger"> Löschen
+                    </button>
+                </div>
+            </div>
+    </div>
 </div>
+<div class=container__login>
+    <div class="container mt-3">
+        <form>
+            <div class="mb-3">
+                <label for="deckName">Fragendeckname:</label>
+                <input type="text" id="deckname" name="deckname" class="form-control">
+            </div>
+            <div class="mb-3">
+            <label for="modul">Wähle ein Modul aus:</label>
+                <select id="modul" name="modul" class="form-select">
 
+                <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "toor";
+                        $dbname = "quiz";
+                        $user_id = $_SESSION['userid'];
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        $sql = "SELECT modulkuerzel FROM modul" ;
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo "<option>" . $row["modulkuerzel"]. "</option>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+
+                        $conn->close();
+                    ?>
+                </select>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="btn btn-outline-success"> Anlegen
+                    </button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-danger"> Cancel
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<footer>
 <?php
     include('footer.php')
 ?>
-
-
+</footer>
 </body>
 </html>
