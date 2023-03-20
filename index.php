1737219e5d00d7ae1e1e41ebe6e4b3ab9df2195e
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-20 18:55:06 +0100

    navbar anpassung

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ if(!isset($_SESSION['userid'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projekt</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
 	
 	
