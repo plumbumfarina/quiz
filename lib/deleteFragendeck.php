33382ea331273c408c08faf317f759374e24e189
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-20 20:18:28 +0100

    Löschen Fragendeck neu

---
+++ lib/deleteFragendeck.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "toor";
+$dbname = "quiz";
+$user_id = $_SESSION['userid'];
+
+$fragendeck_id = $_GET['fragendeck_id'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Prüfung ob ein Fragendeckname angegeben wurde
+if(isset($fragendeck_id)) {
+    $sql = "DELETE FROM fragendeck WHERE fragendeck_id = $fragendeck_ids";
+} else {
+    echo "Kein Fragendeckname angegeben.";
+}
+
+if ($conn->query($sql) === TRUE) {
+  header("Refresh: 1; URL=../decks.php");
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
