cd8dbec3dba5a8587f372555b1968d7072f0fd76
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-20 22:31:40 +0100

    new_table wurde durch fragen ersetzt

--- lib/getFragenAnzahl.php
+++ lib/getFragenAnzahl.php
@@ -5,7 +5,7 @@ function getFragenAnzahl($fragendeck_id){
     if(isset($_SESSION['userid'])) {
 
             $dbconnector = new PDO('mysql:host=localhost;dbname=quiz', 'root', 'toor');
-            $statement = $dbconnector->prepare("SELECT COUNT(*) as fragenAnzahl FROM new_table WHERE fragendeck_id = :fragendeck_id");
+            $statement = $dbconnector->prepare("SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE fragendeck_id = :fragendeck_id");
             $result = $statement->execute(array('fragendeck_id' => $fragendeck_id));
 
             $fragen = $statement->fetch();
