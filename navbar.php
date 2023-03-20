41de686cd8768251acfee046832648e17550b71b
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-20 18:10:31 +0100

    Fragenübersicht entfernt

--- navbar.php
+++ navbar.php
@@ -6,7 +6,6 @@
             <li><a href="index.php">Startseite</a></li>
             <li><a href="decks.php">Meine Kartendecks</a></li>
             <li><a href="startGame.php">Neues Spiel</a></li>
-            <li><a href="fragenUebersicht.php">Fragenübersicht</a></li>
             <a class="navMenu-logout" href="lib/logout.php">LOGOUT</a>
 
         </ul>
