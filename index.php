b001133167c00214c4d99c260dd80b6d2d4cdb69
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-21 17:34:41 +0100

    Buttons wieder eingefügt.

--- index.php
+++ index.php
@@ -26,6 +26,17 @@ if(!isset($_SESSION['userid'])) {
 		include('navbar.php')
 	?>	
   
+  	<div id="start_screen" class="jumbotren quiz_start" >
+		<h1>Herzlich Willkommen zum Quiz</h1>
+		<br>
+		<br>
+		<div class="button-container">
+			<p style="text-align:center"><button id="start_btn" class="btn btn-primary btn-lg start" role="button">Neues Spiel starten</button></p>	  <!-- Der Button muss auf die Seite GameStart führen und die andern Button auch auf die jeweiligen Seiten -->
+			<p style="text-align:center"><button id="kartendecks_btn" onclick="window.location.href='decks.php';" href="decks.php" class="btn btn-primary btn-lg kartendeck" role="button">Kartendecks</button></p>	
+			<p style="text-align:center"><button id="einstellungen_btn" class="btn btn-primary btn-lg einstellungen" role="button">Benutzereinstellungen</button></p>	
+		</div>
+	</div>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	
 	
