a192d72494de2f7a245aac1eab3aa1c57b3167b3
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 11:12:20 +0100

    datenbank verbindung in Variable verschoben.

--- lib/deleteFragendeck.php
+++ lib/deleteFragendeck.php
@@ -1,29 +1,17 @@
 <?php
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "toor";
-$dbname = "quiz";
-$user_id = $_SESSION['userid'];
+include('lib/dbConnector.php');
 
 $fragendeck_id = $_GET['fragendeck_id'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 // Prüfung ob eine Kartendeck_ID angegeben wurde
 if(isset($fragendeck_id)) {
-    // Statement zum löschen aller Fragen des Kartendecks bezüglich Abhängigkeit der Tabelle
+    // Statement zum Löschen aller Fragen des Kartendecks bezüglich Abhängigkeit der Tabelle
     $sqlFragen = "DELETE FROM fragen WHERE fragendeck_id = $fragendeck_id";
     // Ausführung Löschung und Prüfung, ob Löschung der Fragen erfolgreich war
     if ($conn->query($sqlFragen) === TRUE) {
-        // Löschen des entsprechenden Kartendecks
+        // Statement tum Löschen des entsprechenden Kartendecks
         $sqlFragenDeck = "DELETE FROM fragendeck WHERE fragendeck_id = $fragendeck_id";
     }
 } else {
