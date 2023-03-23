10ebe672c749088ffb1c9b91b1cc2f3e7f1255de
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 19:53:53 +0100

    letzte Änderung fragendecl

--- lib/addFrageAndBack.php
+++ lib/addFrageAndBack.php
@@ -3,7 +3,7 @@ session_start();
 
 include('lib/dbConnectorMYSQLI.php');
 
-$fragendeck_id = $_POST['kartendeck_id'];
+$kartendeck_id = $_POST['kartendeck_id'];
 $fragentext = $_POST['fragentext'];
 $antwortEins = $_POST['Antwort1'];
 $antwortZwei = $_POST['Antwort2'];
@@ -12,8 +12,8 @@ $antwortVier = $_POST['Antwort4'];
 $richtigkeit = $_POST['richtigkeit'];
 
 // Prüfung ob ein Fragendeckname angegeben wurde
-if(isset($fragendeck_id)) {
-    $sql = "INSERT INTO fragen (fragentext, kartendeck_id, antwortEins, antwortZwei, antwortDrei, antwortVier, richtigkeit) VALUES ('$fragentext', '$fragendeck_id', '$antwortEins', '$antwortZwei', '$antwortDrei', '$antwortVier', '$richtigkeit')";
+if(isset($kartendeck_id)) {
+    $sql = "INSERT INTO fragen (fragentext, kartendeck_id, antwortEins, antwortZwei, antwortDrei, antwortVier, richtigkeit) VALUES ('$fragentext', '$kartendeck_id', '$antwortEins', '$antwortZwei', '$antwortDrei', '$antwortVier', '$richtigkeit')";
 } else {
     echo "Keine richtige Antwort angegeben.";
 }
