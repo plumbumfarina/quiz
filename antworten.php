ff3dcadac4885ed6f013d5f3058475ed4ba0c24d
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 21:49:45 +0100

    test funktion;

--- antworten.php
+++ antworten.php
@@ -52,6 +52,7 @@ if(!isset($_SESSION['userid'])) {
 			}
 			//shuffle($fragenListe);
 			$anzahlFragen = count($fragenListe);
+            echo $fragenListe[$currentIndex];
 			$currentFrage = getFrage($fragenListe[$currentIndex]);
 			$currentAntwort = getAntwort($fragenListe[$currentIndex]);
 			echo $currentFrage; 
