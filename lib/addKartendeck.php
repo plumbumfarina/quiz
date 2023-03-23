268515407d2404ee12d232d8542fac9d6ffebba9
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 23:55:45 +0100

    Kleine Anpassung

--- lib/addKartendeck.php
+++ lib/addKartendeck.php
@@ -26,7 +26,7 @@ if(isset($deckname)) {
 }
 
 if ($conn->query($sql) === TRUE) {
-  header("Refresh: 0.1; URL=../decks.php");
+  header("Location: URL=../decks.php");
   echo "Neues Kartendeck erfolgreich angelegt. In 5 Sekunden geht es zum Login.";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
