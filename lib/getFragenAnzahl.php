5b1b2deb25c51cdc5ac38033614402ff6f67625f
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 15:43:50 +0100

    test getAnzahl

--- lib/getFragenAnzahl.php
+++ lib/getFragenAnzahl.php
@@ -1,16 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "toor";
-$dbname = "quiz";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include("lib/dbConnector.php");
 
 function getFragenAnzahl($kartendeck_id){
 
