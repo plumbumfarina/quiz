f974a9798ef6e205192aa28b53cf9785977f234a
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 21:13:06 +0100

    connector

--- lib/getFragenAnzahl.php
+++ lib/getFragenAnzahl.php
@@ -3,7 +3,7 @@
 function getFragenAnzahl($kartendeck_id){
    global $conn;
    if(isset($kartendeck_id)) {
-        
+        include_once('lib/dbConnectorPDO.php');
         $statement = $conn->prepare("SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE kartendeck_id = :kartendeck_id");
         $result = $statement->execute(array('kartendeck_id' => $kartendeck_id));
 
