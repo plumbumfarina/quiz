19e7087bac3b30d37e093667657fd69ff490f234
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 21:55:01 +0100

    testFunktion getFrage

--- antworten.php
+++ antworten.php
@@ -26,8 +26,29 @@ if(!isset($_SESSION['userid'])) {
         <h1 class="form__title">Frage</h1>
         <?php
             include_once('lib/dbConnectorMYSQLI.php');
-            include_once('lib/getFrage.php');
-            include_once('lib/getAntworten');
+
+            function getFrage($fragen_id){
+            // Prüfung ob eine angegeben wurde 
+              if(isset($fragen_id)) {
+            // Abfrage aller Informationen einer Frage
+                $sqlFrage = "SELECT fragentext FROM fragen WHERE fragen_id = $fragen_id";
+                $result = $conn->query($sqlFragen);
+                  if ($result->num_rows > 0) {
+            // Ausgabe des Tabelleninhaltes
+                    while($row = $result->fetch_assoc()) {
+                        $fragentext = $row['fragentext'];
+                    } 
+                  } else {
+                    echo "Kein Fragentext gefunden.";
+                  }
+              } else {
+                echo "Keine Frage angegeben.";
+              }
+            
+              return $fragentext;
+            
+            }
+            
 
             $kartendeck_id = 6; //$_GET['kartendeck_id'];
 
