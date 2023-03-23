b7315f41d9b834ac212817192063bc3e861cc92b
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 17:10:51 +0100

    dbconnector

--- lib/dbConnector.php
+++ lib/dbConnector.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "toor";
-    $dbname = "quiz";
+    $dbname = "ProjektQuiz";
     $user_id = $_SESSION['userid'];
 
     // Create connection
