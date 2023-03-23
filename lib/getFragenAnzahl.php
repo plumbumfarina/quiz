4684668e30f0438e851554af5c8a53583ec78473
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 20:57:47 +0100

    connector

--- lib/getFragenAnzahl.php
+++ lib/getFragenAnzahl.php
@@ -1,13 +1,12 @@
 <?php
 
-
+/*
 function getFragenAnzahl($kartendeck_id){
 
    if(isset($kartendeck_id)) {
-      include_once('lib/dbConnectorMYSQLI.php');
-        $sql = "SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE kartendeck_id = :kartendeck_id";
-        
-        $result = $conn->query($sql);
+        include_once('lib/dbConnectorPDO.php');
+        $statement = $conn->prepare("SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE kartendeck_id = :kartendeck_id");
+        $result = $statement->execute(array('kartendeck_id' => $kartendeck_id));
 
         $fragen = $statement->fetch();
         $fragenAnzahl = $fragen['fragenAnzahl'];
@@ -17,5 +16,27 @@ function getFragenAnzahl($kartendeck_id){
     }
 }
 
+*/
+?>
+
+
+<?php
+
+function getFragenAnzahl($kartendeck_id){
+
+    if(isset($kartendeck_id)) {
+        include_once('lib/dbConnectorMYSQLI.php');
+        $statement = $conn->prepare("SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE kartendeck_id = ?");
+        $statement->bind_param("i", $kartendeck_id);
+        $statement->execute();
+        $result = $statement->get_result();
+
+        $fragen = $result->fetch_assoc();
+        $fragenAnzahl = $fragen['fragenAnzahl'];
+        return $fragenAnzahl;
+    } else {
+       echo "ooops ... something went wrong ...";
+    }
+}
 
 ?>
