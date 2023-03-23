1db38f698f586bd77a25c0073d14d06ab366c6d6
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 18:12:25 +0100

    registrieren

--- registrieren.php
+++ registrieren.php
@@ -15,7 +15,17 @@ session_start();
 <?php
 $showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
 
-include('lib/dbConnector.php');
+$servername = "localhost";
+$username = "root";
+$password = "toor";
+$dbname = "ProjektQuiz";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 if(isset($_GET['register'])) {
     $error = false;
