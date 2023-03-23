c120659dc0d90fa09969f402402ca18dba01f20f
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 12:37:34 +0100

    navbar

--- lib/getNickname.php
+++ lib/getNickname.php
@@ -1,19 +1,8 @@
 <?php
-session_start();
-    #include('lib/dbConnector.php');
+    session_start();
+    include('lib/dbConnector.php');
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "toor";
-    $dbname = "quiz";
     $user_id = $_SESSION['userid'];
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
     if(isset($_SESSION['userid'])) {
             #$user_id = $_SESSION['userid'];
