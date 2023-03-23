d21293bfbb95a45941885552413a91672eef8647
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 19:56:08 +0100

    connector

--- lib/addFrage.php
+++ lib/addFrage.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
 
-include('lib/dbConnectorPDO.php');
+$servername = "localhost";
+$username = "root";
+$password = "toor";
+$dbname = "ProjektQuiz";
+$user_id = $_SESSION['userid'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $kartendeck_id = $_POST['kartendeck_id'];
 $fragentext = $_POST['fragentext'];
