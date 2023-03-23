32e24a9aae7406095bee1555fc4d749752a33cbe
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 15:36:02 +0100

    Neue Tabelleninteraktion

--- lib/getFragenAnzahl.php
+++ lib/getFragenAnzahl.php
@@ -16,20 +16,17 @@ function getFragenAnzahl($kartendeck_id){
 
     if(isset($kartendeck_id)) {
 
-            $statement = $conn->prepare("SELECT COUNT(*) as fragenAnzahl FROM fragen WHERE kartendeck_id = :kartendeck_id");
-            $result = $statement->execute(array('kartendeck_id' => $kartendeck_id));
+        $result = mysqli_query($conn, "SELECT * FROM fragen WHERE kartendeck_id = $kartendeck_id");
 
-            $fragen = $statement->fetch();
-            $fragenAnzahl = $fragen['fragenAnzahl'];
+        $num_rows = mysqli_num_rows($result);
 
-            return $fragenAnzahl;
+        mysqli_close($conn);
 
     } else {
-        $fragenAnzahl = 'ERROR';
-
-        return $fragenAnzahl;
+        $num_rows = 'ERROR';
     }
 
+    return $num_rows;
 }
 
 ?>
