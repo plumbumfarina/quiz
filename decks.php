064947a9252bcb4bbe1ef526402ccbd8f1a6edbb
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 15:09:26 +0100

    Fehlersuche

--- decks.php
+++ decks.php
@@ -70,7 +70,7 @@ echo "hab";
                                     <td>" . $row["kartendeck_name"]. "</td>
                                     <td>" . $row["modulkuerzel"]. "</td>
                                     <td>" . $row["modulname"]. "</td>
-                                    <td>" . getFragenAnzahl($row["kartendeck_id"]). "</td>
+                                    <td>234"/* . getFragenAnzahl($row["kartendeck_id"]). */"</td>
                                     <td>" . $row["public"]. "</td>
                                     <td>
                                         <button type='button' class='buttonBearbeiten' value='" . $row["kartendeck_id"]. "' onclick='openBearbeiteKartendeck(" .  $row['kartendeck_id']. ")'> Bearbeiten </button>
