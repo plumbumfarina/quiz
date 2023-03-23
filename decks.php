ba5d52ae890ca18d8bf7d0ff4628a49ba4927e24
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-24 00:02:22 +0100

    schönere Darstellung der public Einträge

--- decks.php
+++ decks.php
@@ -62,7 +62,7 @@ if(!isset($_SESSION['userid'])) {
                                     <td>" . $row["modulkuerzel"]. "</td>
                                     <td>" . $row["modulname"]. "</td>
                                     <td>" . getFragenAnzahl($row["kartendeck_id"]) . "</td>
-                                    <td>" . $row["public"]. "</td>
+                                    <td>" . ($row["public"] ? "Ja" : "Nein") . "</td>
                                     <td>
                                         <button type='button' class='buttonBearbeiten' value='" . $row["kartendeck_id"]. "' onclick='openBearbeiteKartendeck(" .  $row['kartendeck_id']. ")'> Bearbeiten </button>
                                     </td>
