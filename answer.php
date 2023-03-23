8e5844938eda29e31632c3e55e99bc7de49818a5
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-23 19:46:25 +0100

    CSS datei hat gefehlt

--- answer.php
+++ answer.php
@@ -18,7 +18,7 @@ include('lib/getAntworten.php');
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
 	<title>Projekt</title>
 </head>
 <body>
