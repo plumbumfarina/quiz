a0b2e1efad487cc687b851d74aa38ef88d46c5d1
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 14:57:43 +0100

    decks.php

--- decks.php
+++ decks.php
@@ -44,20 +44,20 @@ include('lib/getFragenAnzahl.php');
                 </thead>
                 <tbody>
                     <?php
-                        #include('lib/dbConnector.php');
+                        include('lib/dbConnector.php');
 
-                        $servername = "localhost";
-                        $username = "root";
-                        $password = "toor";
-                        $dbname = "quiz";
-                        $user_id = $_SESSION['userid'];
+                        #$servername = "localhost";
+                        #$username = "root";
+                        #$password = "toor";
+                        #$dbname = "quiz";
+                        #$user_id = $_SESSION['userid'];
 
                         // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        #$conn = new mysqli($servername, $username, $password, $dbname);
                         // Check connection
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
+                        #if ($conn->connect_error) {
+                        #    die("Connection failed: " . $conn->connect_error);
+                        #}
 
                         $sql = "SELECT kartendeck_id, kartendeck_name, kartendeck.modul_id, public, modulkuerzel, modulname FROM kartendeck JOIN modul WHERE (kartendeck.modul_id = modul.modul_id) AND (user_id = $user_id) ORDER BY modulkuerzel ASC";
                         $result = $conn->query($sql);
@@ -115,20 +115,20 @@ include('lib/getFragenAnzahl.php');
                 <select id="modul" name="modul" class="form-select">
 
                 <?php
-                    #include('lib/dbConnector.php');
+                    include('lib/dbConnector.php');
                     
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "toor";
-                    $dbname = "quiz";
-                    $user_id = $_SESSION['userid'];
+                    #$servername = "localhost";
+                    #$username = "root";
+                    #$password = "toor";
+                    #$dbname = "quiz";
+                    #$user_id = $_SESSION['userid'];
 
                     // Create connection
-                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    #$conn = new mysqli($servername, $username, $password, $dbname);
                     // Check connection
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
+                    #if ($conn->connect_error) {
+                    #    die("Connection failed: " . $conn->connect_error);
+                    #}
 
                     $sql = "SELECT modulname FROM modul" ;
                     $result = $conn->query($sql);
