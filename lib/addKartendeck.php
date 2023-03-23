c9d055cfee6c14d0150b5546dccadb3f40c411db
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 19:07:09 +0100

    addKartendeck

--- lib/addKartendeck.php
+++ lib/addKartendeck.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
 
-include('lib/dbConnector.php');
+$servername = "localhost";
+$username = "root";
+$password = "toor";
+$dbname = "ProjektQuiz";
+# $user_id = $_SESSION['userid'];
 
-$user_id = $_SESSION['userid'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $deckname = $_POST['deckname'];
 $modulname = $_POST['modul'];
 $public = $_POST['public'];
