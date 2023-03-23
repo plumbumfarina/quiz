4c0518446987d67d9e7342a5c59467003d8a36b6
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-23 16:38:48 +0100

    decks.php

--- decks.php
+++ decks.php
@@ -4,11 +4,9 @@ session_start();
 if(!isset($_SESSION['userid'])) {
     header('location: login.php');
     die('Bitte zuerst einloggen');
-}
-
+} 
 include('lib/getFragenAnzahl.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,19 +15,16 @@ include('lib/getFragenAnzahl.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Kartendeckübersicht</title>
+    <title>Decks</title>
 </head>
 <body>
-
 <?php
-	include('navbar.php');
+    include('navbar.php')
 ?>
-
 <div>
     <div class="container mt-3">
-        <h1 class="formTitle">Kartendecks</h1>
+        <h1 class="form__title">Kartendecks</h1>
             <table class="table table-striped">
                 <thead class="table-dark">
                     <tr>
@@ -44,38 +39,23 @@ include('lib/getFragenAnzahl.php');
                 </thead>
                 <tbody>
                     <?php
-                        #include('lib/dbConnector.php');
-                        
-                        $servername = "localhost";
-                        $username = "root";
-                        $password = "toor";
-                        $dbname = "quiz";
-                        $user_id = $_SESSION['userid'];
-
-                        // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        // Check connection
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
-
-                        $sql = "SELECT kartendeck_id, kartendeck_name, kartendeck.modul_id, public, modulkuerzel, modulname FROM kartendeck JOIN modul WHERE (kartendeck.modul_id = modul.modul_id) AND (user_id = $user_id) ORDER BY modulkuerzel ASC";
+                        include('lib/dbConnector.php');
+                        $sql = "SELECT fragendeck_id, fragendeck_name, fragendeck.modul_id, public, modulkuerzel, modulname FROM fragendeck JOIN modul WHERE (fragendeck.modul_id = modul.modul_id) AND (user_id = $user_id) ORDER BY modulkuerzel ASC";
                         $result = $conn->query($sql);
-
                         if ($result->num_rows > 0) {
                             // output data of each row
                             while($row = $result->fetch_assoc()) {
                                 echo "<tr>
-                                    <td>" . $row["kartendeck_name"]. "</td>
+                                    <td>" . $row["fragendeck_name"]. "</td>
                                     <td>" . $row["modulkuerzel"]. "</td>
                                     <td>" . $row["modulname"]. "</td>
-                                    <td>" . getFragenAnzahl($row["kartendeck_id"]). "</td>
+                                    <td>" . getFragenAnzahl($row["fragendeck_id"]). "</td>
                                     <td>" . $row["public"]. "</td>
                                     <td>
-                                        <button type='button' class='buttonBearbeiten' value='" . $row["kartendeck_id"]. "' onclick='openBearbeiteKartendeck(" .  $row['kartendeck_id']. ")'> Bearbeiten </button>
+                                        <button type='button' class='btn btn-outline-warning' value='" . $row["fragendeck_id"]. "' onclick='openBearbeiteKartendeck(" .  $row['fragendeck_id']. ")'> Bearbeiten </button>
                                     </td>
                                     <td>
-                                        <button type='button' class='buttonLoeschen' value='" . $row["kartendeck_id"]. "' onclick='openLoescheKartendeck(" .  $row['kartendeck_id']. ")'> Löschen </button>
+                                        <button type='button' class='btn btn-outline-danger' value='" . $row["fragendeck_id"]. "' onclick='openLoescheKartendeck(" .  $row['fragendeck_id']. ")'> Löschen </button>
                                     </td>
                                 </tr>";
                             }
@@ -90,7 +70,6 @@ include('lib/getFragenAnzahl.php');
                                     <td>-</td>
                                 </tr>";
                         }
-
                         $conn->close();
                     ?>
                 </tbody>
@@ -99,8 +78,8 @@ include('lib/getFragenAnzahl.php');
 </div>
 <div>
     <div class="container mt-3">
-        <h1 class="formTitle">Kartendeck hinzufügen</h1>
-        <form action="lib/addKartendeck.php" method="post">
+        <h1 class="form__title">Kartendeck hinzufügen</h1>
+        <form action="lib/addFragendeck.php" method="post">
             <div class="mb-3">
                 <label for="deckName">Kartendeckname:</label>
                 <input type="text" id="deckname" name="deckname" required class="form-control">
@@ -113,62 +92,52 @@ include('lib/getFragenAnzahl.php');
             <div class="mb-3">
             <label for="modul">Wähle ein Modul aus:</label>
                 <select id="modul" name="modul" class="form-select">
-
                 <?php
-                    #include('lib/dbConnector.php');
-                    
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "toor";
-                    $dbname = "quiz";
-                    $user_id = $_SESSION['userid'];
-
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password, $dbname);
-                    // Check connection
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
-
-                    $sql = "SELECT modulname FROM modul" ;
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-                        // output data of each row
-                        while($row = $result->fetch_assoc()) {
-                            echo "<option>" . $row["modulname"]. "</option>";
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "toor";
+                        $dbname = "quiz";
+                        $user_id = $_SESSION['userid'];
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
                         }
-                    } else {
-                        echo "0 results";
-                    }
-
-                    $conn->close();
-                ?>
+                        $sql = "SELECT modulname FROM modul" ;
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo "<option>" . $row["modulname"]. "</option>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        $conn->close();
+                    ?>
                 </select>
             </div>
             <div class="row">
                 <div class="col">
-                    <button type="submit" class="buttonHinzufuegen"> Hinzufügen
+                    <button type="submit" class="btn btn-outline-success"> Anlegen
                     </button>
                 </div>
             </div>
         </form>
     </div>
 </div>
-
 <script>
 function openBearbeiteKartendeck(id) {
-  window.location.href = "fragenUebersicht.php?kartendeck_id=" + id;
+  window.location.href = "fragenUebersicht.php?fragendeck_id=" + id;
 }
 function openLoescheKartendeck(id) {
-  window.location.href = "lib/deleteFragendeck.php?kartendeck_id=" + id;
+  window.location.href = "lib/deleteFragendeck.php?fragendeck_id=" + id;
 }
 </script>
 
 <?php
-	include('footer.php');
+	include('footer.php')
 ?>
-
 </body>
 </html>
-
