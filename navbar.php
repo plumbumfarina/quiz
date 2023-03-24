2f2223ad043af90ab9c8e1cd9741d0e23d5884b3
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-24 13:19:54 +0100

    neuer Versuch

--- navbar.php
+++ navbar.php
@@ -2,16 +2,13 @@
     <ul>
         <li><a href="index.php">Startseite</a></li>
         <li><a href="decks.php">Meine Kartendecks</a></li>
-        <li><a href="startGame.php">Neues Spiel</a></li>
-
-        <?php 
-            session_start();
-            if(isset($_SESSION['nickname'])) {
-                echo " <li>Hallo <b>" . $_SESSION['nickname']."</b>!</li>";
-            } 
-        ?>
-        
+        <li><a href="startGame.php">Neues Spiel</a></li>        
         <a class="navMenu-logout" href="lib/logout.php">LOGOUT</a>
-
     </ul>
+    <?php 
+        session_start();
+        if(isset($_SESSION['nickname'])) {
+            echo " <p>Hallo <b>" . $_SESSION['nickname']."</b>!</p>";
+         } 
+    ?>
 </nav>
