02759676d7b762282613d8a61cfb409e9688d504
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-24 13:17:27 +0100

    Hallo als li

--- navbar.php
+++ navbar.php
@@ -3,12 +3,14 @@
         <li><a href="index.php">Startseite</a></li>
         <li><a href="decks.php">Meine Kartendecks</a></li>
         <li><a href="startGame.php">Neues Spiel</a></li>
+
+        <?php 
+            if(isset($_SESSION['user_id'])) {
+                echo " <li>Hallo <b>" . $_SESSION['nickname']."</b>!</li>";
+            } 
+        ?>
+        
         <a class="navMenu-logout" href="lib/logout.php">LOGOUT</a>
 
     </ul>
-    <?php 
-        if(isset($_SESSION['user_id'])) {
-            echo " <p>Hallo <b>" . $_SESSION['nickname']."</b>!</p>";
-        } 
-    ?>
 </nav>
