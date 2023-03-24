b4609a1537adfab1edcc5774a61095f8910b9f40
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-24 23:49:53 +0100

    zu viel gelöscht

--- imprint.php
+++ imprint.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Projekt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
     <title>Impressum</title>
 </head>
 <body>
