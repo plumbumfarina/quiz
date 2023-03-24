f9440b09d798c6a7153dc1bdd90700db4d0f1234
sonne030 <p.sonnemann@gmail.com>
2023-03-24 09:35:43 +0100

    decks.php Auskommentieren Bootstrap

--- decks.php
+++ decks.php
@@ -4,7 +4,7 @@ session_start();
 if(!isset($_SESSION['userid'])) {
     header('location: login.php');
     die('Bitte zuerst einloggen');
-} 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,8 +13,8 @@ if(!isset($_SESSION['userid'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
     <title>Decks</title>
 </head>
 <body>
