121bc441a06c6d14051dc9b82c9d81945d659243
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-25 20:08:26 +0100

    optik

--- finaleUebersicht.php
+++ finaleUebersicht.php
@@ -116,7 +116,7 @@ if(!isset($_SESSION['userid'])) {
                         }
                         // Berechnung der Prozentual richtig beantworteten Fragen + Ausgabe
                         $percentage = ($correctAnswers / $totalQuestions) * 100;
-                        echo '<div style="display:flex;" class="ContainerDeck">
+                        echo '<div style="display:flex;align-items: flex-end;" class="ContainerDeck">
                             <p> Du hast </p>
                             <h1>'. $correctAnswers .'</h1>
                             <p> von </p>
