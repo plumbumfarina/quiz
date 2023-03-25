8c91c91bc419b8f3c4011e13a5cd4fe9df556bcf
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-25 17:16:43 +0100

    divdivdivdivdiv

--- lib/logout.php
+++ lib/logout.php
@@ -7,6 +7,9 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Logout</title>
 	<link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
 </head>
