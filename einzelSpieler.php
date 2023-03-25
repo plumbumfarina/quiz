1bb6fc3ab314f13cf857990cea8edde3e860b9c5
hooouy <118460712+hooouy@users.noreply.github.com>
2023-03-25 17:42:11 +0100

    test

--- einzelSpieler.php
+++ einzelSpieler.php
@@ -24,28 +24,39 @@ session_start();
     </header>
 	<main>
         <h1 class="formTitle">Übersicht der Fragen</h1>
-            <?php
+        
+            <table class="TabelleDeck">
+                <thead  class="THeadDeck">
+                    <tr>
+                        <th class="THDeck">Frage</th>
+                        <th class="THDeck">Beantwortet</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
                         
 // Datenbank-Verbindung
-                $servername = "localhost";
-                $username = "root";
-                $password = "toor";
-                $dbname = "ProjektQuiz";
-                // Create connection
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "toor";
+                        $dbname = "ProjektQuiz";
+                    
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
 // wichtige Variablen 
-                $kartendeck_id = $_GET['kartendeck_id'];
-                $_SESSION['kartendeck_id'] = $kartendeck_id;
-                $user_id = $_SESSION['userid'];
-                $fragenListe = array();
-                $antworten = array();
-                $currentIndex = 0;
+                        $kartendeck_id = $_GET['kartendeck_id'];
+                        $_SESSION['kartendeck_id'] = $kartendeck_id;
+                        $user_id = $_SESSION['userid'];
+                        $fragenListe = array();
+                        $antworten = array();
+                        $currentIndex = 0;
+
 //SQL Abfrage für alle Fragen-IDs
-                        $sql = "SELECT fragen_id FROM fragen WHERE kartendeck_id = $kartendeck_id";
+                        $sql = "SELECT fragen_id, fragentext FROM fragen WHERE kartendeck_id = $kartendeck_id";
                         $stmt = $conn->prepare($sql);
                         $stmt->bind_param("i", $kartendeck_id);
                         $stmt->execute();
@@ -67,9 +78,46 @@ session_start();
                         $selectedAnswer = array();
                         $_SESSION['selectedAnswer'] = $selectedAnswer;
 
+// Funktion um die aktuelle Frage herauszufinden
+                        function getFrage($conn, $fragen_id){
+                        // Prüfung ob eine Fragen-ID angegeben wurde 
+                        if(isset($fragen_id)) {
+                            $sqlFrage = "SELECT fragentext FROM fragen WHERE fragen_id = $fragen_id";
+                            $resultFrage = $conn->query($sqlFrage);
+    
+                            if (!$resultFrage) {
+                                printf("Error: %s\n", mysqli_error($conn));
+                                exit();
+                            }
+    
+                            if($resultFrage->num_rows == 1){
+                                $rowFrage = $resultFrage->fetch_assoc();
+                                $fragentext = $rowFrage['fragentext']; 
+                            } else {
+                                echo "Fehler: Die Abfrage gibt das falsche Ergebnis zurück!";
+                            }
+                        } else {
+                            echo "Keine Frage angegeben.";
+                        }
+    
+                        return $fragentext;
+                    }
+
+//Tabelle erstellen mit Fragen IDs und der Antworten
+                        for($i = 0; $i < count($fragenListe); $i++) {
+                            $antworten[$i] = '0';
+                            echo "<tr class='TRDeck'>
+                                <td>Frage " . ($i + 1) . "</td>
+                                <td>" . getFrage($fragenListe[$i])."<td>
+                            </tr>";
+                        }
+
                         $conn->close();
                     ?>
-        <div class="ContainerDeck" style="padding:20px;">
+                </tbody>
+            </table>
+        <br>
+        <div class="ContainerDeck">
             <button type="button" class="buttonSpielen" onclick="openStart('<?php echo $fragenListe[0]; ?>')"> Quiz beginnen! </button>
         </div>
     </main>
