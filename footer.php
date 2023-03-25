dd95f074d135fcdbb6a82ce492058a53337aa6bb
sonne030 <p.sonnemann@gmail.com>
2023-03-25 11:41:17 +0100

    Footer Überschrift CSS

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <footer>
-	<ul class="footer-list">
-		<a href="settings.php">Allgemeine Einstellungen</a><br>
-		<a href="cookies.php">Cookie Einstellungen</a><br>
-		<a href="contact.php">Kontakt</a><br>
-		<a href="privacy.php">Privatsphäre-Einstellungen</a><br>
-		<a href="imprint.php">Impressum</a><br>
-	 </ul>
+	<div class="footer-container"> <!-- Neues div-Element hinzugefügt -->
+		<h3 class="footer-heading">IU Quiz</h3>
+			<ul class="footer-list">
+				<a href="settings.php">Allgemeine Einstellungen</a><br>
+				<a href="cookies.php">Cookie Einstellungen</a><br>
+				<a href="contact.php">Kontakt</a><br>
+				<a href="privacy.php">Privatsphäre-Einstellungen</a><br>
+				<a href="imprint.php">Impressum</a><br>
+			 </ul>
 </footer>
