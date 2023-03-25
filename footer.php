bcdc26679748c57e03a24f9fafe1a06ab429f673
sonne030 <p.sonnemann@gmail.com>
2023-03-25 10:20:07 +0100

    Footer CSS: linksbündig

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <footer>
-	<a href="settings.php">Allgemeine Einstellungen</a><br>
-	<a href="cookies.php">Cookie Einstellungen</a><br>
-	<a href="contact.php">Kontakt</a><br>
-	<a href="privacy.php">Privatsphäre-Einstellungen</a><br>
-	<a href="imprint.php">Impressum</a><br>
+	<ul class="footer-list">
+		<a href="settings.php">Allgemeine Einstellungen</a><br>
+		<a href="cookies.php">Cookie Einstellungen</a><br>
+		<a href="contact.php">Kontakt</a><br>
+		<a href="privacy.php">Privatsphäre-Einstellungen</a><br>
+		<a href="imprint.php">Impressum</a><br>
+	 </ul>
 </footer>
