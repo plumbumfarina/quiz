670e97d5af65d56c9a89eb3efc06ae840f679041
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-25 17:21:30 +0100

    divdivdivdivdiv

--- lib/logout.php
+++ lib/logout.php
@@ -6,41 +6,41 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Logout</title>
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
-</head>
-<body>
-    <header>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../style.css" type="text/css" media="screen"/>
+        <title>Logout</title>
+    </head>
+    <body>
+        <header>
+            <?php
+                include_once('lib/navbar.php')
+            ?>
+        </header>
+        <div class="containerLogin">
+            <h1 class="formTitle">Logout</h1>
+            <p>Are you sure you want to logout?</p>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
         <?php
-            include_once('lib/navbar.php')
+        // Prüfung ob Logout geclickt wurde
+        if(isset($_POST['logout'])) {
+            // Session zerstören
+            session_start();
+            session_destroy();
+            
+            sleep(0.1);
+            // Zur Startseite weiterleiten
+            header("Location: ../login.php");
+            exit;
+        }
         ?>
-    </header>
-	<div class="containerLogin">
-		<h1 class="formTitle">Logout</h1>
-		<p>Are you sure you want to logout?</p>
-		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-			<button type="submit">Logout</button>
-		</form>
-	</div>
-    <?php
-	// Prüfung ob Logout geclickt wurde
-	if(isset($_POST['logout'])) {
-		// Session zerstören
-		session_start();
-		session_destroy();
-		
-        sleep(0.1);
-		// Zur Startseite weiterleiten
-		header("Location: ../login.php");
-		exit;
-	}
-	?>
-    <?php
-		include_once('footer.php')
-	?>
-</body>
+        <?php
+            include_once('lib/footer.php')
+        ?>
+    </body>
 </html>
