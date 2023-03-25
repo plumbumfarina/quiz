c674c5d5850475567ad420a51d45e2352bf092b6
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-25 11:22:25 +0100

    currentIndex

--- antworten.php
+++ antworten.php
@@ -135,7 +135,7 @@ if(!isset($_SESSION['userid'])) {
                         
         ?>
 
-        <form action="?weiter=1" method="post">
+        <form action="?answer=1" method="post">
         <p>Selected answer: <?php echo !empty($selectedAnswer) ? implode(', ', $selectedAnswer) : ''; ?></p>
 
             <p><?php foreach ($selectedAnswer as $antwort_id) {echo $antwort_id . " ";} ?></p>
