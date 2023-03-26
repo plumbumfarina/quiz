b0f2c3974a78b02e5f0ff7fd0e80b7c59beae134
Lukas Bleymehl <lukas.bleymehl@iubh-fernstudium.de>
2023-03-26 07:50:46 +0200

    links korrigiert

--- lib/footer.php
+++ lib/footer.php
@@ -2,7 +2,6 @@
 	<div class="footer-container"> <!-- Neues div-Element hinzugefügt -->
 		<h3 class="footer-heading">IU Quiz</h3>
 			<ul class="footer-list">
-				<a href="settings.php">Allgemeine Einstellungen</a><br>
 				<a href="cookies.php">Cookie Einstellungen</a><br>
 				<a href="contact.php">Kontakt</a><br>
 				<a href="privacy.php">Privatsphäre-Einstellungen</a><br>
